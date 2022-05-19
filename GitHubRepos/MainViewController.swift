24e1b444ea0930a7bf0cacaa8f03e3f00a8ee0ea
Сергей Бец <serjbets@serjbets.com>
2022-05-19 11:28:58 +0200

    Create and config empty project

---
+++ GitHubRepos/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubRepos
+//
+//  Created by Сергей Бец on 19.05.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
